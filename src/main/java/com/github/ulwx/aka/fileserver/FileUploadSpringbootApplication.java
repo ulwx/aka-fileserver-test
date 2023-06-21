b55af616fdd2232999bb9af47263e82c091b83ca
ulwx <39908619@qq.com>
2023-06-21 11:05:21 +0800

    更新

---
+++ src/main/java/com/github/ulwx/aka/fileserver/FileUploadSpringbootApplication.java
@@ -0,0 +1,11 @@
+package com.github.ulwx.aka.fileserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadSpringbootApplication  {
+    public static void main(String[] args) throws Exception{
+        SpringApplication.run(FileUploadSpringbootApplication.class, args);
+    }
+}
